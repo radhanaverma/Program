935dba3654b083114e7c6320bb1ef2e67781a12a
Radhana Verma <radhanaverma331@gmail.com>
2025-09-19 06:49:25 +0530

    Add files via upload

---
+++ pracctical44.php
@@ -0,0 +1,16 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $msg = $_POST['message'];
+    header("Location: practical44.html?name=" . urlencode($name));
+    exit();
+}
+if (isset($_GET['name'])) {
+    $student = $_GET['name'];
+    echo "<h2>Thank you, $student! Your feedback has been submitted.</h2>";
+}
+
+
+
+
